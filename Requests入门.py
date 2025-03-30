c1c630c81ccbb5069b7a0a86c01b576aa07d53cf
lyz20130112 <lyz013112@outlook.com>
2025-03-30 19:04:08 +0800

    Update Requests入门.py

--- Requests入门.py
+++ Requests入门.py
@@ -5,7 +5,7 @@ query = input()
 url = f'https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=1&rsv_idx=1&tn=baidu&wd={query}'
 
 dic ={
-    'user-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36'
+    'user-agent' : 'your user-agent'
 }
 resp = requests.get(url,headers=dic)
 
